93b24adbb985dc4a72e296904bd4e2541258ec38
Zelone <jiteshmob1@gmail.com>
2022-05-07 05:43:26 +0100

    testing input datas

--- test1.py
+++ test1.py
@@ -10,14 +10,37 @@ def main(WIDTH,HEIGHT):
     TITLE_TEXT = 'Minesweeper'
     WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))#,flag=pygame.OPENGL)
     pygame.display.set_caption(TITLE_TEXT)
-
+    CLOCK= pygame.time.Clock()
     run = True
     while run:
         for event in pygame.event.get():
+            print('N',end='')
             if event.type == pygame.QUIT:
+                print('Q:',event)
                 run = False
                 break
-
+            #print(event.type)
+            if event.type == pygame.KEYDOWN:
+                print(event)
+                print(event.unicode,event.key)
+                print(pygame.mouse.get_pos())
+                print(pygame.mouse.get_rel())#from previous position
+            if event.type == pygame.KEYUP:
+                print(event)
+                print(event.unicode,event.key)
+                print(pygame.mouse.get_pos())
+                print(pygame.mouse.get_rel())#from previous position
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(event)
+                print('DOWN',event.button)#1:leftclick, 2:middleclick, 3:rightclick, 4:scrollup, 5:scrolldown
+                print(event.pos)
+                print(pygame.mouse.get_rel())#from previous position 
+            if event.type == pygame.MOUSEBUTTONUP:
+                print(event)
+                print('UP',event.button)#1:leftclick, 2:middleclick, 3:rightclick, 4:scrollup, 5:scrolldown
+                print(event.pos)
+                print(pygame.mouse.get_rel())#from previous position you started clicking
+        CLOCK.tick(60)
         draw(WINDOW,BG_COLOR)
     pygame.quit()
 
